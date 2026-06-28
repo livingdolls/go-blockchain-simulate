75e921161ca620eb328f1d60d9bf5adb3268e28c
livingdolls <nanangyorozuya@gmail.com>
2026-06-28 08:47:51 +0700

    fix: missing background import in NotificationScreen

--- YuteCoin/app/src/main/java/com/takahashi/yutecoin/ui/dashboard/NotificationScreen.kt
+++ YuteCoin/app/src/main/java/com/takahashi/yutecoin/ui/dashboard/NotificationScreen.kt
@@ -1,5 +1,6 @@
 package com.takahashi.yutecoin.ui.dashboard
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
